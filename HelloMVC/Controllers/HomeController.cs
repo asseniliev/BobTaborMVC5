758386e9f907187bdf0a5d13cdeafe6bd8c21f74
asseniliev <assen.s.iliev@gmail.com>
2023-04-17 00:49:43 +0200

    Create basic MVC5 project Create HomeController, Index view

---
+++ HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Car> cars = HelloModel.GetCars();
+
+            ViewBag.MyValue = "Hello MVC";
+            ViewBag.Cars = HelloModel.GetCars();
+            return View(cars);
+        }
+    }
+}
